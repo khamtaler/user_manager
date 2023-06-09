5b29364accb8aca639269934b681d0ab30a31d39
Tomasz Sroka <tsroka41@gmail.com>
2023-06-09 18:23:27 +0200

    padding on user list

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex min-h-[100vh] items-center justify-center">
+  <div class="flex min-h-[100vh] items-center justify-center py-[50px]">
     <RouterView />
   </div>
 </template>

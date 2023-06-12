7108f45e28e8e1e6f59c46aeb0c8afd31c348100
Tomasz Sroka <tsroka41@gmail.com>
2023-06-12 12:32:23 +0200

    adding suspense blocks for async conent

--- src/views/EditUserDetails.vue
+++ src/views/EditUserDetails.vue
@@ -1,79 +1,84 @@
 <template>
   <div class="mx-auto w-full max-w-[1200px] px-5">
     <PageHeader title="Update User" />
-    <div class="flex flex-col-reverse items-stretch justify-between gap-5 md:flex-row">
-      <div class="flex w-full flex-col rounded-md bg-white p-4 md:flex-[5]">
-        <div class="mt-[50px] flex flex-col justify-evenly gap-5 md:flex-row">
-          <div class="flex w-full flex-1 flex-col">
-            <label for="FirstName" class="mb-2 font-semibold"> First Name </label>
-            <input
-              id="FirstName"
-              type="text"
-              v-model="form.first_name"
-              required
-              :placeholder="form.first_name ? form.first_name : ''"
-              class="rounded-sm border-[1px] border-lighter-gray border-opacity-20 px-4 py-1"
-            />
+    <Suspense>
+      <div class="flex flex-col-reverse items-stretch justify-between gap-5 md:flex-row">
+        <div class="flex w-full flex-col rounded-md bg-white p-4 md:flex-[5]">
+          <div class="mt-[50px] flex flex-col justify-evenly gap-5 md:flex-row">
+            <div class="flex w-full flex-1 flex-col">
+              <label for="FirstName" class="mb-2 font-semibold"> First Name </label>
+              <input
+                id="FirstName"
+                type="text"
+                v-model="form.first_name"
+                required
+                :placeholder="form.first_name ? form.first_name : ''"
+                class="rounded-sm border-[1px] border-lighter-gray border-opacity-20 px-4 py-1"
+              />
+            </div>
+            <div class="flex w-full flex-1 flex-col">
+              <label for="LastName" class="mb-2 font-semibold"> Last Name</label>
+              <input
+                id="LastName"
+                type="text"
+                v-model="form.last_name"
+                :placeholder="form.last_name ? form.last_name : ''"
+                required
+                class="rounded-sm border-[1px] border-lighter-gray border-opacity-20 px-4 py-1"
+              />
+            </div>
           </div>
-          <div class="flex w-full flex-1 flex-col">
-            <label for="LastName" class="mb-2 font-semibold"> Last Name</label>
-            <input
-              id="LastName"
-              type="text"
-              v-model="form.last_name"
-              :placeholder="form.last_name ? form.last_name : ''"
-              required
-              class="rounded-sm border-[1px] border-lighter-gray border-opacity-20 px-4 py-1"
-            />
-          </div>
+          <button
+            type="submit"
+            @click="UpdateUser"
+            class="mx-auto mt-[50px] self-start rounded-md bg-green px-4 py-2 text-white md:mx-0 md:mt-auto"
+          >
+            Update User
+          </button>
         </div>
-        <button
-          type="submit"
-          @click="UpdateUser"
-          class="mx-auto mt-[50px] self-start rounded-md bg-green px-4 py-2 text-white md:mx-0 md:mt-auto"
-        >
-          Update User
-        </button>
-      </div>
-      <div class="flex flex-col bg-white p-4 md:flex-[2]">
-        <figure class="my-[50px]">
-          <img
-            :src="form.avatar ? form.avatar : '/avatar.jpg'"
-            :alt="
-              /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.test(
-                form.avatar
-              )
-                ? 'avatar placeholder'
-                : 'invalid url'
+        <div class="flex flex-col bg-white p-4 md:flex-[2]">
+          <figure class="my-[50px]">
+            <img
+              :src="form.avatar ? form.avatar : '/avatar.jpg'"
+              :alt="
+                /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.test(
+                  form.avatar
+                )
+                  ? 'avatar placeholder'
+                  : 'invalid url'
+              "
+              class="mx-auto block aspect-square min-w-[50%] max-w-[35%] rounded-full border-2 border-light-gray object-cover p-[1px] text-center leading-[150px]"
+            />
+          </figure>
+          <button
+            v-if="!changeAvatar"
+            type="button"
+            @click="
+              () => {
+                changeAvatar = !changeAvatar
+              }
             "
-            class="mx-auto block aspect-square min-w-[50%] max-w-[35%] rounded-full border-2 border-light-gray object-cover p-[1px] text-center leading-[150px]"
-          />
-        </figure>
-        <button
-          v-if="!changeAvatar"
-          type="button"
-          @click="
-            () => {
-              changeAvatar = !changeAvatar
-            }
-          "
-          class="rounded-md border-[1px] border-dark-gray py-1 text-dark-gray"
-        >
-          <font-awesome-icon icon="fa-solid fa-camera" /> Change Photo
-        </button>
-        <div v-if="changeAvatar" class="flex flex-col">
-          <label for="Avatar" class="mb-2 font-semibold">Avatar Url</label
-          ><input
-            type="url"
-            v-model="form.avatar"
-            id="Avatar"
-            placeholder="https://example.com"
-            pattern="https://.*"
-            class="rounded-sm border-[1px] border-lighter-gray border-opacity-20 px-4 py-1"
-          />
+            class="rounded-md border-[1px] border-dark-gray py-1 text-dark-gray"
+          >
+            <font-awesome-icon icon="fa-solid fa-camera" /> Change Photo
+          </button>
+          <div v-if="changeAvatar" class="flex flex-col">
+            <label for="Avatar" class="mb-2 font-semibold">Avatar Url</label
+            ><input
+              type="url"
+              v-model="form.avatar"
+              id="Avatar"
+              placeholder="https://example.com"
+              pattern="https://.*"
+              class="rounded-sm border-[1px] border-lighter-gray border-opacity-20 px-4 py-1"
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <template #fallback class="flex min-h-screen w-full items-center justify-center text-center">
+        Loading...
+      </template>
+    </Suspense>
   </div>
 </template>
 
--- src/views/UsersList.vue
+++ src/views/UsersList.vue
@@ -1,50 +1,57 @@
 <template>
   <div class="mx-auto w-full max-w-[1200px] px-5">
     <PageHeader title="User list" />
-    <div class="rounded-md border-[1px] border-opacity-10 bg-white px-4 py-7">
-      <div
-        class="mb-5 flex flex-col items-center justify-between gap-4 border-b-[1px] border-b-light-gray border-opacity-10 pb-7 md:flex-row"
-      >
-        <BaseSearchBar />
-        <router-link
-          :to="{ name: 'create-user' }"
-          class="rounded-full bg-green px-5 py-2 text-white"
-          ><font-awesome-icon icon="fa-solid fa-plus " class="pr-1 font-bold" /> Add User
-        </router-link>
+    <Suspense>
+      <div>
+        <div class="rounded-md border-[1px] border-opacity-10 bg-white px-4 py-7">
+          <div
+            class="mb-5 flex flex-col items-center justify-between gap-4 border-b-[1px] border-b-light-gray border-opacity-10 pb-7 md:flex-row"
+          >
+            <BaseSearchBar />
+            <router-link
+              :to="{ name: 'create-user' }"
+              class="rounded-full bg-green px-5 py-2 text-white"
+              ><font-awesome-icon icon="fa-solid fa-plus " class="pr-1 font-bold" /> Add User
+            </router-link>
+          </div>
+          <div v-if="usersData">
+            <ListOfUsers :users="usersData.data" />
+          </div>
+        </div>
+        <!-- w zależności od ilości użytkowników należałoby ukrywać strzałki, gdy nie można nimi przejść na kolejną kartę, na potrzebę zobaczenia przycisków zostawiłem je widoczne z zablokowanym przejściem -->
+        <div class="mt-5 flex flex-wrap items-center">
+          <button
+            type="button"
+            @click="getPage(usersData.page - 1)"
+            class="paged-list--button border-[1px] border-lighter-gray bg-white p-2 text-xs"
+          >
+            <font-awesome-icon icon="fa-solid fa-chevron-left" />
+            <font-awesome-icon icon="fa-solid fa-chevron-left" />
+          </button>
+          <button
+            type="button"
+            v-for="(item, index) in usersData.total_pages"
+            :key="item"
+            @click="getPage(index + 1)"
+            :class="{ active: index + 1 === usersData.page }"
+            class="paged-list--button border-[1px] border-l-0 border-lighter-gray bg-white px-3 py-2 text-xs"
+          >
+            {{ index + 1 }}
+          </button>
+          <button
+            type="button"
+            @click="getPage(usersData.page + 1)"
+            class="paged-list--button border-[1px] border-l-0 border-lighter-gray bg-white p-2 text-xs"
+          >
+            <font-awesome-icon icon="fa-solid fa-chevron-right" />
+            <font-awesome-icon icon="fa-solid fa-chevron-right" />
+          </button>
+        </div>
       </div>
-      <div v-if="usersData">
-        <ListOfUsers :users="usersData.data" />
-      </div>
-    </div>
-    <!-- w zależności od ilości użytkowników należałoby ukrywać strzałki, gdy nie można nimi przejść na kolejną kartę, na potrzebę zobaczenia przycisków zostawiłem je widoczne z zablokowanym przejściem -->
-    <div class="mt-5 flex flex-wrap items-center">
-      <button
-        type="button"
-        @click="getPage(usersData.page - 1)"
-        class="paged-list--button border-[1px] border-lighter-gray bg-white p-2 text-xs"
-      >
-        <font-awesome-icon icon="fa-solid fa-chevron-left" />
-        <font-awesome-icon icon="fa-solid fa-chevron-left" />
-      </button>
-      <button
-        type="button"
-        v-for="(item, index) in usersData.total_pages"
-        :key="item"
-        @click="getPage(index + 1)"
-        :class="{ active: index + 1 === usersData.page }"
-        class="paged-list--button border-[1px] border-l-0 border-lighter-gray bg-white px-3 py-2 text-xs"
-      >
-        {{ index + 1 }}
-      </button>
-      <button
-        type="button"
-        @click="getPage(usersData.page + 1)"
-        class="paged-list--button border-[1px] border-l-0 border-lighter-gray bg-white p-2 text-xs"
-      >
-        <font-awesome-icon icon="fa-solid fa-chevron-right" />
-        <font-awesome-icon icon="fa-solid fa-chevron-right" />
-      </button>
-    </div>
+      <template #fallback class="flex min-h-screen w-full items-center justify-center text-center">
+        Loading...
+      </template>
+    </Suspense>
   </div>
 </template>
 
